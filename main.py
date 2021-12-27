11d3d1ca565b549f1a62180c4369ebec2ddc8f7e
Nhoco <gabrielpjrtgames@gmail.com>
2021-12-27 18:05:45 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-print('COMANDOS: 1 - calculadorazinha (soma, subtrai, multiplica ou divide números) \n mais comandos serão adicionados quando eu tiver um tempinho ')
+print('COMANDOS: 1 - calculadorazinha (soma, subtrai, multiplica ou divide números) 2- Conversor de temperatura \n mais comandos serão adicionados quando eu tiver um tempinho ')
 
 comando = input('\n opa! bem vindo! selecione um comando :D \n')
 
@@ -38,3 +38,17 @@ if comando == '1':
     multi = num1 * num2
     print('certo! hmm...')
     print(multi)
+elif comando == '2':
+  grau = input('\n Coloque qual é o grau de temperatura que tu quer converter para o inverso dele. (Por exemplo, F para F° para C° e vice-versa \n')
+  if grau == 'F':
+    farofa = int(input('\n Coloque um número em F° agora, eu irei converter para C°! \n'))  
+    print('Vamos ver...')
+    portioli = 5 * ((farofa - 32) / 9)
+
+    print(portioli)
+  elif grau == 'C':
+    portioli = int(input('\n Coloque um número em C° agora, eu irei converter para F°! \n'))
+     
+    print('Vamos ver...')
+    farofa = (portioli * (9 / 5) + 32)
+    print(farofa)
